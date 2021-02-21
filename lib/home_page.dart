27310881f2253d92010b2f3897dfff8f73ee4735
ABEU KPIDI FREDERIC <frederic.abeu@sm-ic.com>
2021-02-21 10:39:34 +0000

    division du code et changer de page

---
+++ lib/home_page.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int index = 0;
+  int numero = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.indigo,
+        title: Text("Flutter App Testing"),
+        centerTitle: true,
+        // actions: [
+        //   Icon(Icons.account_circle),
+        // ],
+      ),
+      body: Column(
+        children: [
+          Image.network(
+              "https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg"),
+          IconButton(
+            icon: Icon(Icons.add_box),
+            onPressed: () {
+              print("Yoda");
+              setState(() {
+                numero = numero + 1;
+              });
+            },
+          ),
+          Text(numero.toString()),
+          IconButton(icon: Icon(Icons.arrow_right),
+          onPressed: (){})
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.pink,
+        unselectedItemColor: Colors.orange,
+        onTap: (variableInt) {
+          setState(() {
+            index = variableInt;
+          });
+        },
+        currentIndex: index,
+        backgroundColor: Colors.indigo,
+        items: [
+          BottomNavigationBarItem(
+            title: Text('Account'),
+            icon: Icon(Icons.account_circle),
+          ),
+          BottomNavigationBarItem(
+            title: Text('Notification'),
+            icon: Icon(Icons.add_alert),
+          ),
+        ],
+      ),
+    );
+  }
+}
